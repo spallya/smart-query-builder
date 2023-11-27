bdc8ecac9967618621581197fad59c59301cb26d
srinivasdingari <srinu2k7@gmail.com>
2023-11-27 23:18:39 +0530

    appId dropdown changes

--- app/query-builder/src/Search.jsx
+++ app/query-builder/src/Search.jsx
@@ -3,10 +3,12 @@ import axios from "axios";
 
 export default function Search() {
   const [searchTxt, setSearchTxt] = useState("");
+  const [appId, setAppId] = useState("");
+  const [appIdDisplay, setAppIdDisplay] = useState("");
   const [searchStack, setSearchStack] = useState([]);
   const [searchQAArr, setSearchQAArr] = useState([]);
     const arrRef = useRef(null);
-    
+
   useEffect(()=> {
     if(searchQAArr.length > 0) {
         console.log("re render ui");
@@ -16,19 +18,25 @@ export default function Search() {
   const handleValueChange = (e) => {
     console.log("in handle value change: ", e);
     setSearchTxt(e.target.value);
-    
+
+  }
+
+  const handleAppIdChange = (e) => {
+    console.log("in handle value change: ", e);
+    setAppId(e.target.value);
+    setAppIdDisplay(true);
   }
 
   const postSearchResults = () => {
     const url = "http://localhost:8080/ai/dbquery";
-    const body = { userMessage: searchTxt};
+    const body = { appId: appId, userMessage: searchTxt};
     let arr = searchQAArr;
-    /*arr.push({id: arr.length, question: searchTxt, answer: "test"}); // remove when calling api
+    /*arr.push({id: arr.length, question: searchTxt, answer: "test", appId: appId}); // remove when calling api
     arr.reverse(); // remove when calling api
     setSearchQAArr(arr); // remove when calling api */
 
     console.log("in post search results: ", arr);
-   axios.post(url, body)
+    axios.post(url, body)
     .then((response) => {
       console.log("response is: ", response);
       const resp = response.data.completion;
@@ -42,7 +50,7 @@ export default function Search() {
       arr.reverse();
       setSearchStack(arr);
     });
-    
+
   }
 
   const handleSearch = () => {
@@ -59,6 +67,8 @@ export default function Search() {
   const handleReset = () => {
     console.log("in handle reset");
     setSearchTxt("");
+    setAppId("");
+    setAppIdDisplay(false);
     setSearchStack([]);
     setSearchQAArr([]);
   }
@@ -66,12 +76,23 @@ export default function Search() {
 
   return (
     <div className="App">
+
       <h1>Smart Query Builder</h1>
-      <input style={{width: '80%', padding: '5px' }} 
-      placeholder="Enter the text to Search" 
+
+      <div>
+      <select className="item" value={appId} onChange={handleAppIdChange} disabled={appIdDisplay}>
+      <option value="none">Select an application</option>
+      <option value="vantage">Vantage</option>
+      <option value="WCA">WCA</option>
+      <option value="wima">WIMA</option>
+      </select>
+      </div>
+
+      <input style={{width: '80%', padding: '5px' }}
+      placeholder="Enter the text to Search"
       name="searchTxt"
-      value={searchTxt} 
-      
+      value={searchTxt}
+
       onChange={handleValueChange} />
       <div>
       <button name="searchBtn" onClick={handleSearch} style={{padding: '5px', marginRight: '20px', width: '30%'}}>Search</button>
@@ -80,7 +101,7 @@ export default function Search() {
 
       {searchQAArr.length > 0 && (
         <>
-          <h3>Search Results - {searchQAArr.length}</h3>
+          <h3>Search Results for {appId} - {searchQAArr.length}</h3>
           {searchQAArr.reverse().map((item, index) => {
             return (<div>
 <ul><strong>
